cdfa1e00b95b6b0c8cac31a17273844f5de00a0c
unknown <oleggalushh@gmail.com>
2019-09-24 01:28:40 +0300

    HW1_Task4

---
+++ src/test/java/HW1_Task4.java
@@ -0,0 +1,9 @@
+public class HW1_Task4 {
+    public static double a = 5;
+    public static double b = 10;
+
+    public static void  main(String[] args){
+        double result = a / b;
+        System.out.println(result);
+    }
+}
