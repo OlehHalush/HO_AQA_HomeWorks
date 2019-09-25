3a998a324e8bf20c90fd6f6bcc457cc14aa29b39
Oleh Halush <oleh.halush@ctdev.io>
2019-09-25 16:21:11 +0300

    HW3_Task11

---
+++ src/test/java/HW3_Task11.java
@@ -0,0 +1,29 @@
+/*
+Write a program which prints the next figure to the console:
+          *
+         ***
+        *****
+       *******
+      *********
+     ***********
+ */
+
+public class HW3_Task11 {
+    public static void main(String[] args) {
+        int triangleHeight = 3;
+
+        for (int i = 1; i <= triangleHeight; i++) {
+            for (int j = 1; j <= triangleHeight - 1 + i; j++) {
+                //Write spaces before asterisks
+                if (j <= triangleHeight - i) {
+                    System.out.print(" ");
+                }
+                //Write asterisks
+                if (j > triangleHeight - i) {
+                    System.out.print("*");
+                }
+            }
+            System.out.println("");
+        }
+    }
+}
