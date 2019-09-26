188cb84b02cc87bfe597d611032dc4d467a6cce7
Oleh Halush <oleh.halush@ctdev.io>
2019-09-26 11:55:43 +0300

    HW3_Task6

--- src/test/java/HW3_Task6.java
+++ src/test/java/HW3_Task6.java
@@ -1,20 +1,21 @@
+/*
+Write a method named getEvenDigitSum with one parameter of the type "int".
+The method should return the sum of even digits within the number.
+If the number is negative, method should return -1;
+ */
+
 public class HW3_Task6 {
     public static void main(String[] args) {
-        System.out.println(getEvenDigitSum(100));
+        System.out.println(getEvenDigitSum(1001));
     }
 
     public static int getEvenDigitSum(int a) {
-        int result = 0;
-        if (a < 0) {
-            return -1;
-        } else {
-            int sum = 0;
-            for (int i = a; i >= 0; i--) {
-                if (i % 2 == 0) {
-                    sum += i;
-                }
-            }
-            return sum;
+        int sum = 0;
+        while(a > 0 ){
+            int digit = a % 10;
+            a = a / 10;
+            sum += digit;
         }
+        return sum;
     }
 }
